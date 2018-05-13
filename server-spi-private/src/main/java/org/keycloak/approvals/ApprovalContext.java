0355b1572faf4357d3aff6a730ef3fc2724d45df
vmuzikar <vaclav@muzikari.cz>
2018-05-13 21:14:29 +0200

    Javadoc pt.1

--- server-spi-private/src/main/java/org/keycloak/approvals/ApprovalContext.java
+++ server-spi-private/src/main/java/org/keycloak/approvals/ApprovalContext.java
@@ -1,10 +1,10 @@
 /*
- * Copyright 2017 Red Hat, Inc. and/or its affiliates
- *  and other contributors as indicated by the @author tags.
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -24,34 +24,82 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * The context for currently processed action (e.g. creating a user). Carries all the necessary information (like
+ * representations and models) for evaluating an action, creating an approval request, approving an action etc.
+ *
+ * @see ApprovalEvaluator
+ * @see ApprovalHandler
+ *
  * @author Vaclav Muzikar <vmuzikar@redhat.com>
  */
 public class ApprovalContext {
+    /**
+     * The key for storing a representation attribute.
+     */
     public static final String REPRESENTATION_ATTR = "representation";
+
+    /**
+     * The key for storing a model attribute.
+     */
     public static final String MODEL_ATTR = "model";
 
     private RealmModel realm;
     private ApprovalAction action;
     private Map<String, Object> attributes = new HashMap<>();
 
+    /**
+     * Context with no preset attributes.
+     *
+     * @param realm the realm where the action is taking place
+     * @param action the processed action
+     */
     public ApprovalContext(RealmModel realm, ApprovalAction action) {
         setRealm(realm);
         setAction(action);
     }
 
+    /**
+     * Creates a context with preset representation attribute.
+     *
+     * @param realm the realm where the action is taking place
+     * @param action the processed action
+     * @param representation the representation of some entity (e.g. {@link org.keycloak.representations.idm.UserRepresentation})
+     * @return The newly created context.
+     */
     public static ApprovalContext withRepresentation(RealmModel realm, ApprovalAction action, Object representation) {
         return (new ApprovalContext(realm, action)).setRepresentation(representation);
     }
 
+    /**
+     * Creates a context with preset model attribute.
+     *
+     * @param realm the realm where the action is taking place
+     * @param action the processed action
+     * @param model the model for some entity (e.g. {@link org.keycloak.models.UserModel})
+     * @return The newly created context.
+     */
     public static ApprovalContext withModel(RealmModel realm, ApprovalAction action, Object model) {
         return (new ApprovalContext(realm, action)).setModel(model);
     }
 
+    /**
+     * Stores an attribute in this context.
+     *
+     * @param name the key for storing the attribute
+     * @param value the attribute
+     * @return This context.
+     */
     public ApprovalContext setAttribute(String name, Object value) {
         attributes.put(name, value);
         return this;
     }
 
+    /**
+     * Retrieves an attribute stored in this context.
+     *
+     * @param name the key for storing the attribute
+     * @return The retrieved attribute or null if no attribute is stored in the context for given key.
+     */
     public Object getAttribute(String name) {
         return attributes.get(name);
     }
--- server-spi-private/src/main/java/org/keycloak/approvals/ApprovalHandlerFactory.java
+++ server-spi-private/src/main/java/org/keycloak/approvals/ApprovalHandlerFactory.java
@@ -1,10 +1,10 @@
 /*
- * Copyright 2017 Red Hat, Inc. and/or its affiliates
- *  and other contributors as indicated by the @author tags.
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
